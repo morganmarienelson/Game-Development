ba5484d2ec898a06e7c5319517304402178e85b2
morganmarienelson <87725126+morganmarienelson@users.noreply.github.com>
2023-06-29 13:26:24 -0500

    continuinng to work on project 1

--- project1/src/main.js
+++ project1/src/main.js
@@ -1,11 +1,27 @@
-'use strict';
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH= canvas.width = 600;
+const  CANVAS_HEIGHT = canvas.height = 600;
+const playerImage = new Image();
+playerImage.src = "https://www.frankslaboratory.co.uk/downloads/shadow_dog.png";
+const spriteWidth = 575;
+const spriteHeight= 523;
+let frameX = 0;
+let frameY = 0;
+let gameFrame = 0;
+const staggerFrames = 5;
 
-function counter() {
-  let seconds = 0;
-  setInterval(() => {
-    seconds += 1;
-    document.getElementById('app').innerHTML = `<p>You have been here for ${seconds} seconds.</p>`;
-  }, 1000);
-}
+function animate(){
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    let position = Math.floor(gameFrame/staggerFrames) % 6;
+    frameX = spriteWidth * position;
+    ctx.drawImage(playerImage, frameX, frameY* spriteHeight, spriteWidth, spriteHeight, 0 ,0, spriteWidth, spriteHeight);
+   if (gameFrame % staggerFrames == 0){
+        if (frameX < 4) frameX++;
+        else frameX = 0;
+    }
+    gameFrame++;
+    requestAnimationFrame(animate);
+};
+animate();
 
-counter();
