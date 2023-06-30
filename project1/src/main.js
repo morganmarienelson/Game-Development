f76f5b297bbdca8afbc0c0c35980832f16b80358
morganmarienelson <87725126+morganmarienelson@users.noreply.github.com>
2023-06-30 05:20:59 -0500

    git ignore updated

--- project1/src/main.js
+++ project1/src/main.js
@@ -1,21 +1,21 @@
 let playerState = 'run';
 const dropdown = document.getElementById('animations');
-dropdown.addEventListener('change', function(e){
+dropdown.addEventListener('change', function (e) {
     playerState = e.target.value;
 })
 
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH= canvas.width = 600;
-const  CANVAS_HEIGHT = canvas.height = 600;
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
 const playerImage = new Image();
 playerImage.src = "https://www.frankslaboratory.co.uk/downloads/shadow_dog.png";
 const spriteWidth = 575;
-const spriteHeight= 523;
+const spriteHeight = 523;
 let gameFrame = 0;
 const staggerFrames = 5;
 const spriteAnimations = [];
-const animationStates= [
+const animationStates = [
     {
         name: 'idle',
         frames: 7,
@@ -61,7 +61,7 @@ animationStates.forEach((state, index) => {
     let frames = {
         loc: [],
     }
-    for (let j = 0; j < state.frames; j++){
+    for (let j = 0; j < state.frames; j++) {
         let positionX = j * spriteWidth;
         let positionY = index * spriteHeight;
         frames.loc.push({x: positionX, y: positionY});
@@ -69,13 +69,13 @@ animationStates.forEach((state, index) => {
     spriteAnimations[state.name] = frames;
 })
 
-function animate(){
+function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations[playerState].loc.length;
+    let position = Math.floor(gameFrame / staggerFrames) % spriteAnimations[playerState].loc.length;
     let frameX = spriteWidth * position;
     let frameY = spriteAnimations[playerState].loc[position].y;
-    ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0 ,0, spriteWidth, spriteHeight);
-   if (gameFrame % staggerFrames == 0){
+    ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    if (gameFrame % staggerFrames == 0) {
         if (frameX < 4) frameX++;
         else frameX = 0;
     }
