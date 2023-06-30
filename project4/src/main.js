758efb7d2380b113f3fe180d59d814e8a3dcd266
morganmarienelson <87725126+morganmarienelson@users.noreply.github.com>
2023-06-30 14:09:11 -0500

    on click explosion functionality complete

--- project4/src/main.js
+++ project4/src/main.js
@@ -7,27 +7,57 @@ let canvasPosition = canvas.getBoundingClientRect();
 
 class Explosion {
     constructor(x, y) {
-        this.x = x;
-        this.y = y;
         this.spriteWidth = 200;
         this.spriteHeight = 179;
         this.width = this.spriteWidth / 2;
         this.height = this.spriteHeight / 2;
+        this.x = x;
+        this.y = y;
         this.image = new Image();
         this.image.src = 'https://www.frankslaboratory.co.uk/downloads/boom.png';
         this.frame = 0;
+        this.timer = 0;
+        this.angle = Math.random() * 6.2;
     }
 
     update() {
-        this.frame++;
+        this.timer++;
+        if (this.timer % 10 === 0) {
+            this.frame++;
+        }
+        ;
     }
 
     draw() {
-        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle)
+        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, 0 - this.width / 2, 0 - this.height / 2, this.width, this.height);
+        ctx.restore();
     }
 }
 
 window.addEventListener('click', function (e) {
-    let positionX = e.x - canvasPosition.left - 25;
-    let positionY = e.y - canvasPosition.top - 25;
+    createAnimation(e);
 })
+
+function createAnimation(e) {
+    let positionX = e.x - canvasPosition.left;
+    let positionY = e.y - canvasPosition.top;
+    explosions.push(new Explosion(positionX, positionY));
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    for (let i = 0; i < explosions.length; i++) {
+        explosions[i].update();
+        explosions[i].draw();
+        if (explosions[i].frame > 5) {
+            explosions.splice(i, 1);
+            i--;
+        }
+    }
+    requestAnimationFrame(animate);
+}
+
+animate();
