dd58e1404731ed1c1d05265dde9009589764bed1
morganmarienelson <87725126+morganmarienelson@users.noreply.github.com>
2023-06-30 08:01:25 -0500

    beginning parallax background

--- project2/src/script.js
+++ project2/src/script.js
@@ -7,7 +7,7 @@ let gameSpeed = 15;
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = '../backgroundLayers/layer-1.png';
 const backgroundLayer2 = new Image();
-backgroundLayer2.src = 'file:///Users/morgannelson/Desktop/GameDevelopment/project2/backgroundLayers/layer-1.png';
+backgroundLayer2.src = '../backgroundLayers/layer-2.png';
 const backgroundLayer3 = new Image();
 backgroundLayer3.src = '../backgroundLayers/layer-3.png';
 const backgroundLayer4 = new Image();
@@ -20,7 +20,7 @@ let x2 = 2400;
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(backgroundLayer2, x, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.drawImage(backgroundLayer2, x, 0);
     ctx.drawImage(backgroundLayer2, x2, 0);
     if (x < -2400) x = 2400 + x2 - gameSpeed;
     else x -= gameSpeed;
