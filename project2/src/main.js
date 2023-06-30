8bc0f50c23dc5db691e304514cbe15aca3f3f8c6
morganmarienelson <87725126+morganmarienelson@users.noreply.github.com>
2023-06-30 10:14:37 -0500

    create enemy factory

--- project2/src/main.js
+++ project2/src/main.js
@@ -1,11 +1,34 @@
-'use strict';
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+CANVAS_WIDTH = canvas.width = 500;
+CANVAS_HEIGHT = canvas.height = 600;
 
-function counter() {
-  let seconds = 0;
-  setInterval(() => {
-    seconds += 1;
-    document.getElementById('app').innerHTML = `<p>You have been here for ${seconds} seconds.</p>`;
-  }, 1000);
+class Enemy {
+    constructor() {
+        this.x = 10;
+        this.y = 50;
+        this.width = 100;
+        this.height = 100;
+    }
+
+    update() {
+        this.x++;
+        this.y++;
+    }
+
+    draw() {
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+};
+
+const enemy1 = new Enemy();
+const enemy2 = new Enemy();
+
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    requestAnimationFrame(animate);
 }
 
-counter();
+animate();
+
