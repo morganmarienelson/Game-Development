5d40d232cf395a7c8b25abac52299dbd9e38a243
morganmarienelson <87725126+morganmarienelson@users.noreply.github.com>
2023-06-30 08:08:06 -0500

    bg layer file paths updated

--- project1/src/parallaxBackgrounds/parallaxBackground.js
+++ project1/src/parallaxBackgrounds/parallaxBackground.js
@@ -5,15 +5,15 @@ const CANVAS_HEIGHT = canvas.height = 700;
 let gameSpeed = 15;
 
 const backgroundLayer1 = new Image();
-backgroundLayer1.src = '../backgroundLayers/layer-1.png';
+backgroundLayer1.src = '../../backgroundLayers/layer-1.png';
 const backgroundLayer2 = new Image();
-backgroundLayer2.src = 'file:///Users/morgannelson/Desktop/GameDevelopment/project2/backgroundLayers/layer-1.png';
+backgroundLayer2.src = '../../backgroundLayers/layer-2.png';
 const backgroundLayer3 = new Image();
-backgroundLayer3.src = '../backgroundLayers/layer-3.png';
+backgroundLayer3.src = '../../backgroundLayers/layer-3.png';
 const backgroundLayer4 = new Image();
-backgroundLayer4.src = '../backgroundLayers/layer-4.png';
+backgroundLayer4.src = '../../backgroundLayers/layer-4.png';
 const backgroundLayer5 = new Image();
-backgroundLayer5.src = "../backgroundLayers/layer-5.png";
+backgroundLayer5.src = "../../backgroundLayers/layer-5.png";
 
 let x = 0;
 let x2 = 2400;
