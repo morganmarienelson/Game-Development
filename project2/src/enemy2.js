df5adbdd60f9e3615320e0587638fa61b05abb54
morganmarienelson <87725126+morganmarienelson@users.noreply.github.com>
2023-06-30 12:43:14 -0500

    enemy 2 file created

---
+++ project2/src/enemy2.js
@@ -0,0 +1,53 @@
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+CANVAS_WIDTH = canvas.width = 500;
+CANVAS_HEIGHT = canvas.height = 600;
+const numberOfEnemies = 100;
+const enemiesArray = [];
+let gameFrame = 0;
+
+class Enemy {
+    constructor() {
+        this.image = new Image();
+        this.image.src = 'https://www.frankslaboratory.co.uk/downloads/97/enemy_bat_2.png  ';
+        this.speed = Math.random() * 4 + 1;
+        this.spriteWidth = 238;
+        this.spriteHeight = 167;
+        this.width = this.spriteWidth / 1.5;
+        this.height = this.spriteHeight / 1.5;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+    }
+
+    update() {
+        this.x += Math.random() * 3 - 1.5;
+        this.y += Math.random() * 3 - 1.5;
+        if (gameFrame % this.flapSpeed === 0) {
+            this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
+    }
+
+    draw() {
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+    }
+};
+
+for (let i = 0; i < numberOfEnemies; i++) {
+    enemiesArray.push(new Enemy());
+}
+
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    enemiesArray.forEach(enemy => {
+        enemy.update();
+        enemy.draw();
+    })
+    gameFrame++;
+    requestAnimationFrame(animate);
+}
+
+animate();
+
