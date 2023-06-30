46801b466ff7badd1cfe1dc0bb4e218164c8807b
morganmarienelson <87725126+morganmarienelson@users.noreply.github.com>
2023-06-30 11:05:17 -0500

    flap action pattern

--- project2/src/main.js
+++ project2/src/main.js
@@ -3,30 +3,48 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 600;
+const numberOfEnemies = 100;
+const enemiesArray = [];
+let gameFrame = 0;
 
 class Enemy {
     constructor() {
-        this.x = 10;
-        this.y = 50;
-        this.width = 100;
-        this.height = 100;
+        this.image = new Image();
+        this.image.src = 'https://www.frankslaboratory.co.uk/downloads/97/enemy_bat_1.png';
+        this.spriteWidth = 83;
+        this.spriteHeight = 44;
+        this.width = this.spriteWidth / 1.5;
+        this.height = this.spriteHeight / 1.5;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
     }
 
     update() {
-        this.x++;
-        this.y++;
+        this.x += Math.random() * 3 - 1.5;
+        this.y += Math.random() * 3 - 1.5;
+        if (gameFrame % this.flapSpeed === 0) {
+            this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
     }
 
     draw() {
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 };
 
-const enemy1 = new Enemy();
-const enemy2 = new Enemy();
+for (let i = 0; i < numberOfEnemies; i++) {
+    enemiesArray.push(new Enemy());
+}
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    enemiesArray.forEach(enemy => {
+        enemy.update();
+        enemy.draw();
+    })
+    gameFrame++;
     requestAnimationFrame(animate);
 }
 
