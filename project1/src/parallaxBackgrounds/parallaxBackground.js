8ec1b07b7afe8681b51374b5a3372274a1a978fb
morganmarienelson <87725126+morganmarienelson@users.noreply.github.com>
2023-06-30 09:52:03 -0500

    parallax background working with speed display

--- project1/src/parallaxBackgrounds/parallaxBackground.js
+++ project1/src/parallaxBackgrounds/parallaxBackground.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
-let gameSpeed = 10;
+let gameSpeed = 4;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = 'https://www.frankslaboratory.co.uk/downloads/97/layer-1.png';
@@ -15,54 +15,57 @@ backgroundLayer4.src = 'https://www.frankslaboratory.co.uk/downloads/97/layer-4.
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = "https://www.frankslaboratory.co.uk/downloads/97/layer-5.png";
 
-const slider = document.getElementById('slider');
-slider.value = gameSpeed;
-const showGameSpeed = document.getElementById('showGameSpeed');
-showGameSpeed.innerHtml = gameSpeed;
-slider.addEventListener('change', function (e) {
-    gameSpeed = e.target.value;
+window.addEventListener('load', function () {
+    const slider = document.getElementById('slider');
+    slider.value = gameSpeed;
+    const showGameSpeed = document.getElementById('showGameSpeed');
+    showGameSpeed.innerText = gameSpeed;
+    slider.addEventListener('change', function (e) {
+        gameSpeed = e.target.value;
+    });
+
+
+    class Layer {
+        constructor(image, speedModifier) {
+            this.x = 0;
+            this.y = 0;
+            this.width = 2400;
+            this.height = 700;
+            this.image = image;
+            this.speedModifier = speedModifier;
+            this.speed = gameSpeed * this.speedModifer;
+        }
+
+        update() {
+            this.speed = gameSpeed * this.speedModifier;
+            if (this.x <= -this.width) {
+                this.x = 0;
+            }
+            this.x = this.x - this.speed;
+        }
+
+        draw() {
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+        }
+    }
+
+    const layer1 = new Layer(backgroundLayer1, 0.5);
+    const layer2 = new Layer(backgroundLayer2, 0.5);
+    const layer3 = new Layer(backgroundLayer3, 0.5);
+    const layer4 = new Layer(backgroundLayer4, 0.5);
+    const layer5 = new Layer(backgroundLayer5, 1);
+
+    const gameObjects = [layer1, layer2, layer3, layer4, layer5];
+
+    function animate() {
+        ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        gameObjects.forEach(object => {
+            object.update();
+            object.draw();
+        })
+        requestAnimationFrame(animate);
+    };
+    animate();
 });
 
-
-class Layer {
-    constructor(image, speedModifier) {
-        this.x = 0;
-        this.y = 0;
-        this.width = 2400;
-        this.height = 700;
-        this.image = image;
-        this.speedModifier = speedModifier;
-        this.speed = gameSpeed * this.speedModifer;
-    }
-
-    update() {
-        this.speed = gameSpeed * this.speedModifier;
-        if (this.x <= -this.width) {
-            this.x = 0;
-        }
-        this.x = Math.floor(this.x - this.speed);
-    }
-
-    draw() {
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-        ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
-    }
-}
-
-const layer1 = new Layer(backgroundLayer1, 0.5);
-const layer2 = new Layer(backgroundLayer2, 0.5);
-const layer3 = new Layer(backgroundLayer3, 0.5);
-const layer4 = new Layer(backgroundLayer4, 0.5);
-const layer5 = new Layer(backgroundLayer5, 1);
-
-const gameObjects = [layer1, layer2, layer3, layer4, layer5];
-
-function animate() {
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    gameObjects.forEach(object => {
-        object.update();
-        object.draw();
-    })
-    requestAnimationFrame(animate);
-};
-animate();
