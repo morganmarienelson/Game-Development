1650169a59a34b1b51b9fbc7109d8b37e12125a1
morganmarienelson <87725126+morganmarienelson@users.noreply.github.com>
2023-06-30 13:54:02 -0500

    on click explosion functionality compelte

--- project4/src/main.js
+++ project4/src/main.js
@@ -1,11 +1,33 @@
-'use strict';
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = 500;
+canvas.height = 700;
+const explosions = [];
+let canvasPosition = canvas.getBoundingClientRect();
 
-function counter() {
-  let seconds = 0;
-  setInterval(() => {
-    seconds += 1;
-    document.getElementById('app').innerHTML = `<p>You have been here for ${seconds} seconds.</p>`;
-  }, 1000);
+class Explosion {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.spriteWidth = 200;
+        this.spriteHeight = 179;
+        this.width = this.spriteWidth / 2;
+        this.height = this.spriteHeight / 2;
+        this.image = new Image();
+        this.image.src = 'https://www.frankslaboratory.co.uk/downloads/boom.png';
+        this.frame = 0;
+    }
+
+    update() {
+        this.frame++;
+    }
+
+    draw() {
+        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+    }
 }
 
-counter();
+window.addEventListener('click', function (e) {
+    let positionX = e.x - canvasPosition.left - 25;
+    let positionY = e.y - canvasPosition.top - 25;
+})
