6800d627818850d2bd64bb225d13895268c8e57d
morganmarienelson <87725126+morganmarienelson@users.noreply.github.com>
2023-06-30 06:33:56 -0500

    first background moving demonstration

--- project2/src/main.js
+++ project2/src/main.js
@@ -1,11 +1,11 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width = 500;
-const CANVAS_HEIGHT = canvas.height = 300;
+const CANVAS_WIDTH = canvas.width = 800;
+const CANVAS_HEIGHT = canvas.height = 700;
 let gameSpeed = 15;
 
 const backgroundLayer1 = new Image();
-backgroundLayer1.src = 'https://mir-s3-cdn-cf.behance.net/project_modules/disp/dd1c3d16532917.562ad5374e593.png';
+backgroundLayer1.src = '../backgroundLayers/layer-1.png';
 const backgroundLayer2 = new Image();
 backgroundLayer2.src = '../backgroundLayers/layer-2.png';
 const backgroundLayer3 = new Image();
@@ -16,15 +16,15 @@ const backgroundLayer5 = new Image();
 backgroundLayer5.src = '../backgroundLayers/layer-5.png';
 
 let x = 0;
-let x2 = 400;
+let x2 = 2400;
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(backgroundLayer1, x, 0);
-    ctx.drawImage(backgroundLayer1, x2, 0);
-    if (x < -400) x = 400 + x2 - gameSpeed;
+    ctx.drawImage(backgroundLayer2, x, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.drawImage(backgroundLayer2, x2, 0);
+    if (x < -2400) x = 2400 + x2 - gameSpeed;
     else x -= gameSpeed;
-    if (x2 < -400) x2 = 400 + x - gameSpeed;
+    if (x2 < -2400) x2 = 2400 + x - gameSpeed;
     else x2 -= gameSpeed;
     requestAnimationFrame(animate);
 };
