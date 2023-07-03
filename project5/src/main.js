499933b482289e0ae7a3b2eec1f273a98b9f1ec2
morganmarienelson <87725126+morganmarienelson@users.noreply.github.com>
2023-07-03 09:47:27 -0500

    explosion effect complete

--- project5/src/main.js
+++ project5/src/main.js
@@ -74,22 +74,22 @@ class Explosion {
         this.y = y;
         this.frame = 0;
         this.timeSinceLastFrame = 0;
-        this.frameInterval = 500;
+        this.frameInterval = 150;
         this.markedForDeletion = false;
 
     }
 
     update(deltaTime) {
-        if (this.frame == 0)
-            this.timeSinceLastFrame += deltaTime;
+        this.timeSinceLastFrame += deltaTime;
         if (this.timeSinceLastFrame > this.frameInterval) {
             this.frame++;
+            this.timeSinceLastFrame = 0;
             if (this.frame > 5) this.markedForDeletion = true;
         }
     }
 
     draw() {
-        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y,
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y - this.size / 4,
             this.size, this.size);
     }
 }
